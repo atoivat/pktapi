687c89584ee457ce458ab83b05eb322cf4b1be65
Otávio Leite <otavio.leite.ufu@gmail.com>
2021-07-12 10:28:27 -0300

    improved script to populate database

--- populate.py
+++ populate.py
@@ -1,4 +1,3 @@
-from logging import exception
 import requests as req
 
 from core.database import SessionLocal
@@ -17,26 +16,40 @@ pokemons = res["results"]
 db = SessionLocal()
 
 for pokemon in pokemons:
+    # Request pokemon info
     poke_res = req.get(pokemon['url'])
     if poke_res.status_code != 200:
         print(f"Failed to request {pokemon['name'].capitalize()} data from PokeAPI: status", poke_res.status_code)
         break
-
     poke_info = poke_res.json()
+
+    # Request pokemon species info
     poke_species_res = req.get(f"https://pokeapi.co/api/v2/pokemon-species/{poke_info['id']}")
     if poke_res.status_code != 200:
         print(f"Failed to request {pokemon['name'].capitalize()}({poke_info['id']}) data from PokeAPI: status", poke_res.status_code)
         break
     poke_species = poke_species_res.json()
     
-
+    # Handle pokemon species description language and special characters
+    description = None
+    for flavor_txt_entry in poke_species["flavor_text_entries"]:
+        if flavor_txt_entry["language"]["name"] == "en":
+            description = flavor_txt_entry["flavor_text"]\
+                .replace("\n", " ")\
+                .replace("\u000c", " ")
+            break
+    
+    if description is None:
+        print(f"Failed to fetch {pokemon['name'].capitalize()}({poke_info['id']}) description in english from PokeAPI")
+    
+    # Format data
     pk_data = {
         "id": poke_info['id'],
         "name": pokemon['name'].capitalize(),
         "types": [
             poke_info['types'][0]['type']['name'].capitalize()
         ],
-        "description": poke_species["flavor_text_entries"][0]["flavor_text"],
+        "description": description,
         "weight": poke_info['weight'],
         "sprite": poke_info['sprites']['front_default']
     }
@@ -45,17 +58,19 @@ for pokemon in pokemons:
     except:
         pass
 
+    # Save in DB
     try:
         db_pkdata = PokemonData(**pk_data)
         db.add(db_pkdata)
         db.commit()
         db.refresh(db_pkdata)
         if db_pkdata is None:
-            raise exception
-    except:
-        print(f"Failed to insert {pk_data['name']} data on database. [{exception}] Stoping...")
+            raise Exception
+    except Exception as e:
+        print(f"Failed to insert {pk_data['name']} data on database. [{e}] Stoping...")
         break
 
+    # Print status
     if pk_data['id'] < 151:
         print(f" {pk_data['id']}/151", end='\r', flush=True)
     else:
